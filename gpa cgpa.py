e6040aa6862d5087579beb9b546910bbfba53406
NAVEEN567851 <kummaripallinaveenkumar567@gmail.com>
2025-10-30 03:37:20 -0700

    Add files via upload

---
+++ gpa cgpa.py
@@ -0,0 +1,15 @@
+#Write a python program to create a student class with name, gpa
+#and cgpa instance attributes.
+class st:
+
+    def __init__(s,name,gpa,cgpa):
+        s.name=name
+        s.gpa=gpa
+        s.cgpa=cgpa
+s1=st("csit",9.7,8.5)
+print(s1.name,s1.gpa,s1.cgpa)
+
+ 
+ 
+ 
+
